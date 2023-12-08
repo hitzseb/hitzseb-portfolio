3c6fe96ea6579fc0d372169d98f087bc084f85c0
hitzseb <hitzseb@gmail.com>
2023-12-08 19:24:18 -0300

    v1

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,10 @@
 const Projects = () => {
     return (
         <div className="container my-4">
-            <div className="mx-auto" style={{ maxWidth: '50rem' }}>
+            <div className="mx-auto" style={{ maxWidth: '35rem' }}>
                 <h2>Chainsaw Man API</h2>
-                <p>A RESTful API that grants access to comprehensive information about Chainsaw Man. While initial access through GET requests is open without credentials, there are limitations on request frequency for anonymous users. To unlock extended usage capabilities, users are encouraged to register, enabling the use of JWT for higher request limits.</p>
-                <a href="https://chainsawmanapi.pythonanywhere.com/">Go to Chainsaw Man API</a>
+                <p className="fs-4">A RESTful API that grants access to comprehensive information about Chainsaw Man.</p>
+                <a href="https://chainsawmanapi.pythonanywhere.com/">Check it out</a>
             </div>
         </div>
     );
