02bb2b19f38aaebd32b8e33b06db668264a1e8fb
hitzseb <hitzseb@gmail.com>
2023-12-10 18:57:12 -0300

    responsive

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,7 +3,7 @@ const Projects = () => {
         <div className="container my-4">
             <div className="mx-auto project-card" style={{ maxWidth: '35rem' }}>
                 <h2>Chainsaw Man API</h2>
-                <p className="fs-4">A RESTful API that grants access to comprehensive information about Chainsaw Man.</p>
+                <p className="fs-4 d-none d-md-block">A RESTful API that grants access to comprehensive information about Chainsaw Man.</p>
                 <a href="https://chainsawmanapi.pythonanywhere.com/">Check it out</a>
             </div>
         </div>
