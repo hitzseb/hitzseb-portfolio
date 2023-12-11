27b1d48cd5a87d3b407953c224b9e5b9a7d6450a
hitzseb <hitzseb@gmail.com>
2023-12-10 23:33:57 -0300

    offcanvas

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,25 +4,33 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-md bg-primary" data-bs-theme="dark">
       <div className="container-fluid">
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+
+        <button className="navbar-toggler ms-auto" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2"
+          aria-controls="offcanvasNavbar2" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-
-        <div className="collapse navbar-collapse justify-content-center" id="navbarColor01">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/skills" className="nav-link">Skills</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/projects" className="nav-link">Projects</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/contact" className="nav-link">Contact</Link>
-            </li>
-          </ul>
+        <div className="offcanvas offcanvas-end text-bg-primary" tabindex="-1" id="offcanvasNavbar2"
+          aria-labelledby="offcanvasNavbar2Label">
+          <div className="offcanvas-header">
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas"
+              aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body justify-content-center">
+            <ul className="navbar-nav">
+              <li className="nav-item" data-bs-dismiss="offcanvas">
+                <Link to="/" className="nav-link">Home</Link>
+              </li>
+              <li className="nav-item" data-bs-dismiss="offcanvas">
+                <Link to="/skills" className="nav-link">Skills</Link>
+              </li>
+              <li className="nav-item" data-bs-dismiss="offcanvas">
+                <Link to="/projects" className="nav-link">Projects</Link>
+              </li>
+              <li className="nav-item" data-bs-dismiss="offcanvas">
+                <Link to="/contact" className="nav-link">Contact</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
