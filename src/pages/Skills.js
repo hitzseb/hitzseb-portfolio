3cdfe3f6bee4f0e4b26c039a13d4149184d691f2
hitzseb <hitzseb@gmail.com>
2023-12-12 12:13:09 -0300

    v1

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,18 +1,76 @@
 const Skills = () => {
-    return (
-        <div className="container my-4 text-center">
-            <img className="img-fluid icon" src="/images/java-original.svg" alt="Java" title="Java" />
-            <img className="img-fluid icon" src="/images/spring.svg" alt="Spring" title="Spring Boot" />
-            <img className="img-fluid icon" src="/images/python.svg" alt="Python" title="Python" />
-            <img className="img-fluid icon" src="/images/django.svg" alt="Django" title="Django" />
-            <img className="img-fluid icon" src="/images/javascript-js.svg" alt="JavaScript" title="JavaScript" />
-            <img className="img-fluid icon" src="/images/file-type-css.svg" alt="CSS" title="CSS" />
-            <img className="img-fluid icon" src="/images/file-type-html.svg" alt="HTML" title="HTML" />
-            <img className="img-fluid icon" src="/images/react.svg" alt="React" title="React" />
-            <img className="img-fluid icon" src="/images/bootstrap.svg" alt="Bootstrap" title="Bootstrap" />
-            <img className="img-fluid icon" src="/images/postgresql.svg" alt="PostgreSQL" title="PostgreSQL" />
-        </div>
-    );
-}
+  return (
+    <div className="container my-4 text-center">
+      <div className="mx-auto" style={{ maxWidth: "50rem" }}>
+        <img
+          className="img-fluid icon"
+          src="/images/java-original.svg"
+          alt="Java"
+          title="Java"
+        />
+        <img
+          className="img-fluid icon"
+          src="/images/spring.svg"
+          alt="Spring"
+          title="Spring Boot"
+        />
+        <img
+          className="img-fluid icon"
+          src="/images/python.svg"
+          alt="Python"
+          title="Python"
+        />
+        <img
+          className="img-fluid icon"
+          src="/images/django.svg"
+          alt="Django"
+          title="Django"
+        />
+        <img
+          className="img-fluid icon"
+          src="/images/javascript-js.svg"
+          alt="JavaScript"
+          title="JavaScript"
+        />
+        <img
+          className="img-fluid icon"
+          src="/images/file-type-css.svg"
+          alt="CSS"
+          title="CSS"
+        />
+        <img
+          className="img-fluid icon"
+          src="/images/file-type-html.svg"
+          alt="HTML"
+          title="HTML"
+        />
+        <img
+          className="img-fluid icon"
+          src="/images/react.svg"
+          alt="React"
+          title="React"
+        />
+        <img
+          className="img-fluid icon"
+          src="/images/bootstrap.svg"
+          alt="Bootstrap"
+          title="Bootstrap"
+        />
+        <img
+          className="img-fluid icon"
+          src="/images/file-type-mysql.svg"
+          alt="MySQL"
+          title="MySQL"
+        />
+        <img
+          className="img-fluid icon"
+          src="/images/postgresql.svg"
+          alt="PostgreSQL"
+          title="PostgreSQL"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Skills;
