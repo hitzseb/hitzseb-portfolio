a093f31c2f067db60ae7bab7cccf19e3b51a3189
hitzseb <hitzseb@gmail.com>
2024-07-24 13:48:58 -0300

    fixed _blank

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,9 +4,16 @@ const Projects = () => {
       <div className="mx-auto" style={{ maxWidth: "40rem" }}>
         <h2>Jfinance | yahoo-finance-api</h2>
         <p className="fs-4 d-none d-md-block">
-          A Java library that provides a simple interface to interact with the Yahoo Finance API and retrieve stock and price chart data.
+          A Java library that provides a simple interface to interact with the
+          Yahoo Finance API and retrieve stock and price chart data.
         </p>
-        <a href="https://github.com/hitzseb/yahoo-finance-api" target="_blank">Check it out</a>
+        <a
+          href="https://github.com/hitzseb/yahoo-finance-api"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check it out
+        </a>
       </div>
     </div>
   );
