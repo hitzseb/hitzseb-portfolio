b9830463e6628e03577d05d8572a983d7018c52b
hitzseb <hitzseb@gmail.com>
2024-07-25 20:26:29 -0300

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
 const Home = () => {
   return (
     <div className="container my-5">
-      <div className="mx-auto" style={{ maxWidth: "40rem" }}>
+      <div className="mx-auto" style={{ maxWidth: "35rem" }}>
         <p className="fs-2">Hi there, 👋</p>
-        <p className="fs-2">I'm Sebastian Hitz, a Fullstack Web Developer based in Argentina.</p>
+        <p className="fs-2">I'm Sebastian Hitz, a Fullstack Web Developer.</p>
       </div>
     </div>
   );
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,10 @@
 const Projects = () => {
   return (
     <div className="container my-5">
-      <div className="mx-auto" style={{ maxWidth: "40rem" }}>
+      <div className="mx-auto" style={{ maxWidth: "35rem" }}>
         <h2>Jfinance | yahoo-finance-api</h2>
         <p className="fs-4 d-none d-md-block">
-          A Java library that provides a simple interface to interact with the
-          Yahoo Finance API and retrieve stock and price chart data.
+          A Java library that provides a simple interface to interact with the Yahoo Finance API and retrieve stock data.
         </p>
         <a
           href="https://github.com/hitzseb/yahoo-finance-api"
