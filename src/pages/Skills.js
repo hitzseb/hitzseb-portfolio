e79bf41499ca77c08ef006d0a0ebe98d9f863657
hitzseb <hitzseb@gmail.com>
2024-08-03 18:14:29 -0300

    update

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -2,58 +2,30 @@ import useFadeIn from '../hooks/useFadeIn';
 
 const Skills = () => {
   const fadeInStyle = useFadeIn(1000);
+  const skills = [
+    { src: "/images/java.svg", name: "Java" },
+    { src: "/images/spring.svg", name: "Spring" },
+    { src: "/images/javascript.svg", name: "JavaScript" },
+    { src: "/images/react.svg", name: "React" },
+    { src: "/images/css.svg", name: "CSS" },
+    { src: "/images/html.svg", name: "HTML" },
+    { src: "/images/bootstrap.svg", name: "Bootstrap" },
+    { src: "/images/mysql.svg", name: "MySQL" },
+    { src: "/images/git.svg", name: "Git" }
+  ];
 
   return (
     <div className="container my-5 py-5 text-center" style={fadeInStyle}>
       <div className="mx-auto" style={{ maxWidth: "45rem" }}>
-        <img
-          className="img-fluid icon"
-          src="/images/java-original.svg"
-          alt="Java"
-          title="Java"
-        />
-        <img
-          className="img-fluid icon"
-          src="/images/spring.svg"
-          alt="Spring"
-          title="Spring Boot"
-        />
-        <img
-          className="img-fluid icon"
-          src="/images/javascript-js.svg"
-          alt="JavaScript"
-          title="JavaScript"
-        />
-        <img
-          className="img-fluid icon"
-          src="/images/react.svg"
-          alt="React"
-          title="React"
-        />
-        <img
-          className="img-fluid icon"
-          src="/images/file-type-css.svg"
-          alt="CSS"
-          title="CSS"
-        />
-        <img
-          className="img-fluid icon"
-          src="/images/file-type-html.svg"
-          alt="HTML"
-          title="HTML"
-        />
-        <img
-          className="img-fluid icon"
-          src="/images/bootstrap.svg"
-          alt="Bootstrap"
-          title="Bootstrap"
-        />
-        <img
-          className="img-fluid icon"
-          src="/images/file-type-mysql.svg"
-          alt="MySQL"
-          title="MySQL"
-        />
+        {skills.map((skill, index) => (
+          <img
+            key={index}
+            className="img-fluid icon"
+            src={skill.src}
+            alt={skill.name}
+            title={skill.name}
+          />
+        ))}
       </div>
     </div>
   );
